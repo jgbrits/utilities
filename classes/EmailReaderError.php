27f07df953c0078efadd1bd73fcdca5cc5e75a57
Andre van Zuydam <andrevanzuydam@gmail.com>
2019-08-19 15:19:32 +0200

    Moved defines

--- classes/EmailReaderError.php
+++ classes/EmailReaderError.php
@@ -3,6 +3,27 @@
 
 namespace Utilities;
 
+define("EMAIL_ERROR_IMAP_ERROR", "1");
+define("EMAIL_ERROR_IMAP_STREAM", "2");
+define("EMAIL_ERROR_IMAP_LIST", "3");
+define("EMAIL_ERROR_MAILBOX_FOLDER", "4");
+define("EMAIL_ERROR_MAILBOX_HEADERS", "5");
+define("EMAIL_ERROR_MESSAGE_HEADER", "6");
+define("EMAIL_ERROR_MESSAGE_DATA", "7");
+define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS", "8");
+
+define("EMAIL_ERROR_IMAP_ERROR_MESSAGE", "Imap is not installed");
+define("EMAIL_ERROR_IMAP_STREAM_MESSAGE", "Failed to open imap stream");
+define("EMAIL_ERROR_IMAP_LIST_MESSAGE", "Failed to get a list of mailbox folders");
+define("EMAIL_ERROR_MAILBOX_FOLDER_MESSAGE", "Failed to open specific folder in mailbox");
+define("EMAIL_ERROR_MAILBOX_HEADERS_MESSAGE", "Failed to get mailbox headers");
+define("EMAIL_ERROR_MESSAGE_HEADER_MESSAGE", "Failed to get message header");
+define("EMAIL_ERROR_MESSAGE_DATA_MESSAGE", "Failed to get message data");
+define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS_MESSAGE", "Failed to edit message flags");
+
+define ("NO_ERROR", "No Error");
+
+
 /**
  * Class EmailReaderError
  * @package Utilities
@@ -23,25 +44,6 @@ class EmailReaderError
         $this->code = $code;
         $this->message = $message;
 
-        define("EMAIL_ERROR_IMAP_ERROR", "1");
-        define("EMAIL_ERROR_IMAP_STREAM", "2");
-        define("EMAIL_ERROR_IMAP_LIST", "3");
-        define("EMAIL_ERROR_MAILBOX_FOLDER", "4");
-        define("EMAIL_ERROR_MAILBOX_HEADERS", "5");
-        define("EMAIL_ERROR_MESSAGE_HEADER", "6");
-        define("EMAIL_ERROR_MESSAGE_DATA", "7");
-        define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS", "8");
-
-        define("EMAIL_ERROR_IMAP_ERROR_MESSAGE", "Imap is not installed");
-        define("EMAIL_ERROR_IMAP_STREAM_MESSAGE", "Failed to open imap stream");
-        define("EMAIL_ERROR_IMAP_LIST_MESSAGE", "Failed to get a list of mailbox folders");
-        define("EMAIL_ERROR_MAILBOX_FOLDER_MESSAGE", "Failed to open specific folder in mailbox");
-        define("EMAIL_ERROR_MAILBOX_HEADERS_MESSAGE", "Failed to get mailbox headers");
-        define("EMAIL_ERROR_MESSAGE_HEADER_MESSAGE", "Failed to get message header");
-        define("EMAIL_ERROR_MESSAGE_DATA_MESSAGE", "Failed to get message data");
-        define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS_MESSAGE", "Failed to edit message flags");
-
-        define ("NO_ERROR", "No Error");
 
     }
 
