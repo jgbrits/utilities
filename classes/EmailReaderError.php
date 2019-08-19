b7bf3a86245ad67ae5d06ecdd6fcbf9d54368428
Justin Bruce <justinbruce34@gmail.com>
2019-08-19 14:16:34 +0200

    updated EmailReaderError and took away the "" around the parameters when called in EmailReader

--- classes/EmailReaderError.php
+++ classes/EmailReaderError.php
@@ -11,22 +11,39 @@ namespace Utilities;
 class EmailReaderError
 {
     public $code = false;
+    public $message = false;
 
-    function __construct($code)
+    function __construct($code, $message)
     {
         $this->code = $code;
-        define("EMAIL_ERROR_IMAP_ERROR", "Imap is not installed");
-        define("EMAIL_ERROR_IMAP_STREAM", "Failed to open imap stream");
-        define("EMAIL_ERROR_IMAP_LIST", "Failed to get a list of mailbox folders");
-        define("EMAIL_ERROR_MAILBOX_FOLDER", "Failed to open specific folder in mailbox");
-        define("EMAIL_ERROR_MAILBOX_HEADERS", "Failed to get mailbox headers");
-        define("EMAIL_ERROR_MESSAGE_HEADER", "Failed to get message header");
-        define("EMAIL_ERROR_MESSAGE_DATA", "Failed to get message data");
-        define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS", "Failed to edit message flags");
+        $this->message = $message;
 
-        if (defined($this->code)) {
-            return constant("{$this->code}");
-        } else {
+        define("EMAIL_ERROR_IMAP_ERROR", "1");
+        define("EMAIL_ERROR_IMAP_STREAM", "2");
+        define("EMAIL_ERROR_IMAP_LIST", "3");
+        define("EMAIL_ERROR_MAILBOX_FOLDER", "4");
+        define("EMAIL_ERROR_MAILBOX_HEADERS", "5");
+        define("EMAIL_ERROR_MESSAGE_HEADER", "6");
+        define("EMAIL_ERROR_MESSAGE_DATA", "7");
+        define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS", "8");
+
+        define("EMAIL_ERROR_IMAP_ERROR_MESSAGE", "Imap is not installed");
+        define("EMAIL_ERROR_IMAP_STREAM_MESSAGE", "Failed to open imap stream");
+        define("EMAIL_ERROR_IMAP_LIST_MESSAGE", "Failed to get a list of mailbox folders");
+        define("EMAIL_ERROR_MAILBOX_FOLDER_MESSAGE", "Failed to open specific folder in mailbox");
+        define("EMAIL_ERROR_MAILBOX_HEADERS_MESSAGE", "Failed to get mailbox headers");
+        define("EMAIL_ERROR_MESSAGE_HEADER_MESSAGE", "Failed to get message header");
+        define("EMAIL_ERROR_MESSAGE_DATA_MESSAGE", "Failed to get message data");
+        define("EMAIL_ERROR_EDIT_MESSAGE_FLAGS_MESSAGE", "Failed to edit message flags");
+
+
+
+        function getError()
+        {
+            if ($this->code !== false && $this->message !== false) {
+                return "Error {$this->code}: {$this->message}";
+            }
+
             return false;
         }
     }
